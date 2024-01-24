cfe84a56279418fe56188b37eef63efc2eaaa64e
NikhilJamdar19 <jamdarsnikhil@gmail.com>
2024-01-24 18:05:28 +0530

    Day-2 Commit

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -82,7 +82,7 @@ li {
 }
 
 
-${"" /* resuable code section  */}
+${"" /* reusable code section  */}
 
 .container {
   max-width: 120rem;
@@ -157,7 +157,7 @@ input, textarea{
     cursor: pointer;
     }
 
-@media (max-width: ${({ theme }) => theme.media.tab}) {
+${'' /*@media (max-width: ${({ theme }) => theme.media.tab}) {
     .container {
     max-width: 130rem;
     padding: 0 3.2rem;
@@ -167,7 +167,7 @@ input, textarea{
    @media (max-width: ${({ theme }) => theme.media.mobile}) {
        html {
       font-size: 50%;
-    }
+    }*/}
 
 .grid{
   gap: 3.2rem;
